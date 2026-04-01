60e8e2c2504389e5b82247d16ba843055e32d5e8
p0n1aL <nikitapopov7780@gmail.com>
2026-04-02 00:53:21 +0300

    Изменение переменной Y

--- fixme.cpp
+++ fixme.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     int x = 10;
-    int y = 2;  // Исправление: меняем 0 на 2
+    int y = 5;  // Исправление: меняем 0 на 5
     int z = x / y;
     std::cout << "Result: " << z << std::endl;
     return 0;
